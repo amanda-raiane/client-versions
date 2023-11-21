9f33229b340a570fd35a004e3cc91deca711d628
devprimor <dev@contabilprimor.com.br>
2023-11-21 09:23:38 -0300

    changed version display order

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,23 +16,23 @@ export default function Home() {
         />
       </div>
       <div className="xl:px-96 pt-5 px-8 md:px-16">
-        <h1 className="font-bold text-4xl my-10 px-8 md:px-0">Histórico de mudanças</h1>
-        <div className="py-5 dark:border-b dark:border-slate-800 border-b border-slate-300">
-          <DefaultText
-            title={"adicionamos"}
-            items={a1_0_0}
-            versionDate={"1.0.0 - Lançamento (13/11/23)"}
-          />
-        </div>
+        <h1 className="font-bold text-4xl my-10 px-8 md:px-0">
+          Histórico de mudanças
+        </h1>
+
         <div className="py-5 dark:border-b dark:border-slate-800 border-b border-slate-300">
           <DefaultText
             title={"adicionamos"}
             items={a1_2_1}
             versionDate={"1.2.1 - (21/11/23)"}
           />
-            <DefaultText
-            title={"corrigimos"}
-            items={f1_2_1}
+          <DefaultText title={"corrigimos"} items={f1_2_1} />
+        </div>
+        <div className="py-5 dark:border-b dark:border-slate-800 border-b border-slate-300">
+          <DefaultText
+            title={"adicionamos"}
+            items={a1_0_0}
+            versionDate={"1.0.0 - Lançamento (13/11/23)"}
           />
         </div>
       </div>
