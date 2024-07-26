66fc911c6965451edcd4e576996bb0206b796dcd
amanda-raiane <amanda@codifica.dev>
2024-07-26 09:32:47 -0300

    feat: change date

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
           <div>
             <Image alt="logo escrito G Client" src={logo} className="" />
             <p className="mt-2 dark:text-slate-400 text-sm text-slate-800">
-              24 de julho de 2024 <span>v1.14.0</span>
+              26 de julho de 2024 <span>v1.14.0</span>
             </p>
           </div>
           <div className="flex items-center gap-3">
