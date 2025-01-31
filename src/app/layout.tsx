7797df9021fc90059f1818886efec54eed7ae817
amanda-raiane <amanda@codifica.dev>
2025-01-31 10:23:50 -0300

    chore: add google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 
+import Script from "next/script";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Providers } from "./providers";
@@ -24,8 +25,36 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="GTM"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-W9TVGM9M');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <Providers>
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          {/* Google Tag Manager (noscript) */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-W9TVGM9M"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
+          {/* End Google Tag Manager (noscript) */}
+          {children}
+        </body>
       </Providers>
     </html>
   );
